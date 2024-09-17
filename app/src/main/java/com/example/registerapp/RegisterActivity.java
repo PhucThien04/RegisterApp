117908d438ef6447ce74286a7690443e595d0b5c
PhucThien04 <phucthiensh@gmail.com>
2024-09-17 07:27:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/registerapp/RegisterActivity.java
@@ -0,0 +1,48 @@
+package com.example.registerapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView btn=findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+        EditText inputEmail = findViewById(R.id.inputEmail);
+        EditText inputPassword = findViewById(R.id.inputPassword);
+        Button btnLogin = findViewById(R.id.btnRegister);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                Toast.makeText(RegisterActivity.this, "Email: " + email + "\nPassword: " + password, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
